00646e1a1fd890919db8c6789407e8b6f8b5d2ce
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2023-10-19 14:05:05 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,44 +10,10 @@ an executable
 -- start general pojok code
 local options = {
 	backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
-	clipboard = "unnamedplus", -- Connection to the system clipboard
+  clipboard = "unnamedplus", -- Connection to the system clipboard
 	cmdheight = 0, -- hide command line unless needed
-	completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
-	copyindent = true, -- Copy the previous indentation on autoindenting
-	cursorline = true, -- Highlight the text line of the cursor
-	expandtab = true, -- Enable the use of space in tab
-	fileencoding = "utf-8", -- File content encoding for the buffer
-	fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
-	history = 100, -- Number of commands to remember in a history table
-	ignorecase = true, -- Case insensitive searching
-	laststatus = 3, -- globalstatus
-	lazyredraw = true, -- lazily redraw screen
-	mouse = "a", -- Enable mouse support
-	number = true, -- Show numberline
-	preserveindent = true, -- Preserve indent structure as much as possible
-	pumheight = 10, -- Height of the pop up menu
-	relativenumber = true, -- Show relative numberline
-	scrolloff = 8, -- Number of lines to keep above and below the cursor
-	shiftwidth = 2, -- Number of space inserted for indentation
-	showmode = false, -- Disable showing modes in command line
-	showtabline = 2, -- always display tabline
-	sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
-	signcolumn = "yes", -- Always show the sign column
-	smartcase = true, -- Case sensitivie searching
-	splitbelow = true, -- Splitting a new window below the current one
-	splitright = true, -- Splitting a new window at the right of the current one
-	swapfile = false, -- Disable use of swapfile for the buffer
-	tabstop = 2, -- Number of space in a tab
-	termguicolors = true, -- Enable 24-bit RGB color in the TUI
-	timeoutlen = 300, -- Length of time to wait for a mapped sequence
-	undofile = true, -- Enable persistent undo
-	updatetime = 300, -- Length of time to wait before triggering the plugin
-	wrap = true, -- Disable wrapping of lines longer than the width of window
-	writebackup = false, -- Disable making a backup before overwriting a file
-	-- minimal number of screen columns either side of cursor if wrap is `false`
-	-- guifont = "monospace:h17", -- the font used in graphical neovim applications
-	guifont = "Source_Code_Pro:h17", -- the font used in graphical neovim applications
-	whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line            -- which "horizontal" keys are allowed to travel to prev/next line
+  tabstop = 2, -- Number of space in a tab
+  wrap = true, -- Disable wrapping of lines longer than the width of window
 }
 for k, v in pairs(options) do
 	vim.opt[k] = v
@@ -150,17 +116,7 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
 	"bash",
-	"c",
-	"javascript",
-	"json",
-	"lua",
-	"python",
-	"typescript",
-	"tsx",
-	"css",
-	"rust",
-	"java",
-	"yaml",
+	"lua"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -279,11 +235,10 @@ lvim.builtin.alpha.dashboard.section.footer.val = footer()
 
 lvim.plugins = {
 	{ "ellisonleao/gruvbox.nvim" },
-	{ "manzeloth/live-server" },
 	{ "mg979/vim-visual-multi" },
 	{
 		"CRAG666/code_runner.nvim",
-		requires = "nvim-lua/plenary.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
 		config = function()
 			require("config.coderunner")
 		end,
@@ -367,19 +322,11 @@ lvim.builtin.cmp.mapping = {
 }
 -- lsp installer
 lvim.lsp.installer.setup.ensure_installed = {
-	"sumneko_lua",
 	"jsonls",
-	"html",
-	"cssls",
-	"emmet_ls",
-	"tsserver",
-	"intelephense",
-	"tailwindcss",
 }
 
-require("lvim.lsp.manager").setup("emmet_ls")
-require("lvim.lsp.manager").setup("tailwindcss")
-require("lvim.lsp.manager").setup("intelephense")
+-- require("lvim.lsp.manager").setup("tailwindcss")
+-- require("lvim.lsp.manager").setup("intelephense")
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
